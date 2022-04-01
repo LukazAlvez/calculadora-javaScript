c5b59226835269e2497d1e3aaf72806efe2f1940
Lucas Alves <lc_2014@hotmail.com>
2022-04-01 15:44:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const keysNumber = document.querySelectorAll(".keys");
+const btns = document.querySelectorAll(".btn")
+
+const display = document.getElementById("resultado");
+const historico = document.getElementById("historico");
+
+const apagar = document.getElementById("apagar");
+const igual = document.getElementById("igual")
+
+
+apagar.addEventListener("click", clear)
+igual.addEventListener("click", resultado)
+
+keysNumber.forEach(function(item){
+    item.addEventListener("click", clicou);
+})
+
+btns.forEach(function(item){
+    item.addEventListener("click", clicou )
+})
+
+function clicou (){
+    display.innerHTML += this.innerHTML;
+    historico.innerHTML += this.innerHTML;
+}
+
+function clear (){
+    display.innerHTML = "";
+    historico.innerHTML = "";
+}
+
+
+function resultado(){
+
+    if(display){
+        display.innerHTML = eval(display.innerHTML)
+    }
+
+}
